9e7fd72a81b58beafa659105e058dcd443e56a70
Rakibul islam <himdrakibulislam@gmail.com>
2023-12-06 22:37:08 +0600

    latest

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,20 +5,30 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Pagination\Paginator;
 
 class HomeController extends Controller
 {
     public function home(){
- 
+
+        $perPage = 5; // Number of items per page
+
+        $total = DB::table('books')
+        ->count();
+        $currentPage = Paginator::resolveCurrentPage('page');
+
+        $totalPage = ceil($total / $perPage);
+        
         $books = DB::table('books')
         ->join('author_book', 'books.id', '=', 'author_book.book_id')
         ->join('authors', 'author_book.author_id', '=', 'authors.id')
         ->join('publishers', 'books.publisher_id', '=', 'publishers.id')
         ->select('books.id', 'books.title', 'publishers.name as publisher_name', DB::raw('GROUP_CONCAT(authors.name) as author_names'))
         ->groupBy('books.id', 'books.title', 'publishers.name')
+        ->skip(($currentPage - 1) * $perPage)
+        ->take($perPage)
         ->get();
        
-        return view('home',compact('books'));
+        return view('home',compact('books','totalPage','currentPage'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,17 @@
 @extends('layout.front')
 @section('content')
-    
+    <style>
+        .pagination {
+            display: inline-block;
+        }
+
+        .pagination a {
+            color: black;
+            float: left;
+            padding: 8px 16px;
+            text-decoration: none;
+        }
+    </style>
     <div class="container my-4">
         <h3>Books</h3>
         <hr>
@@ -20,7 +31,15 @@
         @else
             <h3 class="text-center"> <b>No Books Found</b></h3>
         @endif
-       
+
+        <div class="pagination">
+            <a href="{{ $currentPage < 1 ? '#' : '?page=' . $currentPage - 1 }}">&laquo;</a>
+            @for ($i = 1; $i <= $totalPage; $i++)
+                <a
+                    href="{{ '?page=' . $i }}"class="{{ $currentPage == $i ? 'text-white bg-info' : '' }}">{{ $i }}</a>
+            @endfor
+            <a href="{{ $currentPage != $totalPage ? '?page=' . $currentPage + 1 : '#' }} ">&raquo;</a>
+        </div>
     </div>
 @endsection
 @push('extra-script')
